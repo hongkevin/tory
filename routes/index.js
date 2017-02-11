0cfaeb2c838624e3a1d5935a9a8a3d9cf0fb3539
hongseokyou <hsy@zigbang.com>
2017-02-11 18:04:37 +0900

    made send page w/ multer module

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/send', function(req, res, next) {
+  res.render('send', { title: '카카오톡 파일'});
+});
+
+router.post('/send', upload.single('file'), function(req, res, next) {
+  console.log(req.file);
+  console.log(req.body.option);
+  res.send('good job');
 });
 
 module.exports = router;
